7b1497b3503031e2160a8f56aabd32bc61d0d429
sjneubauer <sam.neubauer@trojans.dsu.edu>
2020-10-06 16:57:55 -0500

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int X = 45;
+        int result;
+
+        result = fibRecur(X);
+
+        System.out.println("\n"+result+"\n");
+
+    }
+
+    public static int fibRecur( int X) {
+
+        if ( X == 0 || X == 1 ){
+            return X;
+        } else {
+            return fibRecur(X - 1) + fibRecur( X - 2);        }
+
     }
 }
+
